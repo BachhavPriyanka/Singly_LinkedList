9640c7e378d6b3258c42df09c68199e4898c61ea
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2023-02-25 01:30:58 +0530

    Method for Adding node at first

--- singlyLinkList.go
+++ singlyLinkList.go
@@ -1,5 +1,7 @@
-// Define the LinkedList struct
+// Define a method to add a node to the front of the linked list
 package main
+
+import "fmt"
 
 type Node struct {
 	Data int
@@ -8,4 +10,36 @@ type Node struct {
 
 type LinkedList struct {
 	Head *Node
+}
+
+func (l *LinkedList) AddAtFirst(value int) {
+	newNode := &Node{Data: value}
+
+	if l.Head == nil {
+		l.Head = newNode
+		return
+	}
+	current := l.Head
+
+	for current.Next != nil {
+		current = current.Next
+	}
+	current.Next = newNode
+}
+
+func (list *LinkedList) Display() {
+	current := list.Head
+	for current != nil {
+		fmt.Print(current.Data, " ")
+		current = current.Next
+	}
+	fmt.Println()
+}
+
+func main() {
+	list := &LinkedList{}
+	list.AddAtFirst(1)
+	list.AddAtFirst(2)
+	list.AddAtFirst(3)
+	list.Display()
 }
